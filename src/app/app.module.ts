8f08a5ebc54e00394d2cae4f4fd5fd2297b18e82
aamir saleem <aamir.saleem@iomechs.com>
2017-10-07 20:29:39 +0500

    getting started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
+import { ContactPage } from './../pages/contact/contact';
+import { AboutPage } from './../pages/about/about';
+import { MenuPage } from './../pages/menu/menu';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +16,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    MenuPage,
+    AboutPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    MenuPage,
+    AboutPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
